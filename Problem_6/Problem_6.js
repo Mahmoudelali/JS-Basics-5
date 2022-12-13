031912719cfc0037711b7494d5c85b176faa70ff
Mahmoud elali <Mahmoudelali@github.com>
2022-12-13 23:37:15 +0200

    multiplication table

--- Problem_6/Problem_6.js
+++ Problem_6/Problem_6.js
@@ -1,5 +1,12 @@
-let table  = document.getElementById('table')
+let table = document.getElementById('table')
+let str = '';
 
-for (let i = 1 ; i <= 12 ; i ++) {
-    
+for (let i = 1; i <= 12; i++) {
+    for (let j = 1; j <= 12; j++) {
+        let product = i * j
+        str += `${product} ,`
+    }
+    let br = document.createElement('br');
+    table.innerHTML = str
+    table.appendChild(br)
 }
